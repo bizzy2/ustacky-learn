186b1dea813aeef0425b13769e80a8f6961a14e4
Bismarck Afuzie <bismarck.afuzie@omniswift.com>
2022-05-14 15:35:59 +0100

    Visualizaions for pandas

--- Visualizations.py
+++ Visualizations.py
@@ -1,19 +1,54 @@
 import pandas as pd
-from matplotlib.pyplot import plot
+import matplotlib.pyplot as plt
+import numpy as np
 
+# %%
 '''Data'''
 
 data = pd.read_csv ('studentsdata.csv')
 print (data)
 print ('*' * 50)
-
+# %%
 '''Bar Plot'''
 
 department1 = data.groupby ('department').agg ({'amount_paid': 'mean'})
+
 ## or
+
 department2 = data.groupby (['department'])['amount_paid'].mean ()
 print (department1)
 print ('*' * 50)
 print (department2)
 
-plot (department1)
+p = department1.plot.bar ()
+plt.show ()
+
+# %%
+'''Histogram Plot'''
+
+h = data['amount_paid'].plot.hist ()
+plt.show ()
+
+# %%
+'''Histogram Plot'''
+
+l = data['amount_paid'].plot.line (x='department', y='amount_paid')
+plt.show ()
+
+ln = data.plot.line (x='department', y='amount_paid')
+plt.show ()
+
+# %%
+'''Scatter Plot'''
+
+df = pd.DataFrame (np.random.randn (1000, 4), columns=list ('ABCD'))
+## print (df)
+df.plot.scatter (x='A', y='B')
+plt.show ()
+df.plot.scatter (x='A', y='B', c='C')
+plt.show ()
+df.plot.scatter (x='A', y='B', c='blue')
+plt.show ()
+
+# %%
+'''Hexagonal Plot'''
